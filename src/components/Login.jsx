c414b0b2f2ad374943c1f92e91b7617b105a295f
ASGOOD1 <alexjoaca034@gmail.com>
2025-05-24 15:30:13 +0300

    The modal is now responsive(Auto organises it's tabs base on screen size)

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -12,7 +12,7 @@ export default function Login({ onLogin }) {
         try {
             await axios.post('http://localhost:4000/login', { username, password });
             onLogin();
-            navigate('/pacienti');
+            navigate('/patienti');
         } catch (error) {
             alert('Date de conectare incorecte!');
         }
